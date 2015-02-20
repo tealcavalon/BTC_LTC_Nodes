9b1bc9b81b520e338429751a6f294f95ac13a363
Tealc <tealc@bitmessage.ch>
2015-02-20 21:30:39 +0100

    Update

---
+++ config_ltc.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = array(
+  'debug'                   => FALSE,
+  'rpc_user'                => 'vi ~/.bitcoin/bitcoin.conf fill info here',
+  'rpc_pass'                => 'vi ~/.bitcoin/bitcoin.conf fill info here',
+  'rpc_host'                => 'localhost',
+  'rpc_port'                => '8332',
+  'rpc_ssl'                 => FALSE,
+  'rpc_ssl_ca'              => NULL,
+  'cache_file'              => '/tmp/bitcoind-status.cache',
+  'max_cache_time'          => 300,
+  'nocache_whitelist'       => array('127.0.0.1'),
+  'admin_email'             => 'admin@domain.net',
+  'display_donation_text'   => TRUE,
+  'donation_address'        => 'not_set',
+  'donation_amount'         => '0.001',
+  'use_bitcoind_ip'         => FALSE,
+  'intro_text'              => 'not_set',
+  'display_peer_info'       => FALSE,
+  'display_free_disk_space' => TRUE,
+  'display_ip_location'     => TRUE,
+  'display_testnet'         => TRUE,
+  'display_version'         => TRUE,
+  'use_cache'               => TRUE
+);
+
+?>
